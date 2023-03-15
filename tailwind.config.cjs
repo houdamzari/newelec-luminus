5679e2a51dee5ee0f01834107c0a7222e8e5616e
houdamzari <houda.mzari1999@gmail.com>
2023-03-15 15:08:13 +0100

    Add color variables

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        grey: "#E4E0E0",
+        "textColor-grey": "#616161",
+        "textColor-blue": "#2C8EFF",
+        "textColor-lightGrey": "#B8B8B8",
+
+        orange: "#EB7421",
+      },
+    },
+  },
+  plugins: [],
+};
